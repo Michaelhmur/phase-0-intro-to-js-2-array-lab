10237cf7c13324db7979f38376848e52cfc61d96
Michael Murphy <michaelhmurphy99@gmail.com>
2022-07-09 12:34:01 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 let cats=["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendCat(Ralph) {
     cats.push("Ralph")
 }
+
 function destructivelyPrependCat(Bob) {
     cats.unshift("Bob")
 }
+
 function destructivelyRemoveLastCat() {
     cats.pop();
 }
+
 function destructivelyRemoveFirstCat() {
     cats.shift();
 }
+
 function appendCat (Broom) {
     return [...cats, "Broom"]
-};
+}
+
 function prependCat (Arnold) {
     return ["Arnold", ...cats]
-};
-function removeLastCat () {
-    cats.slice[4];
-    return [...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+
+function removeFirstCat() {
+    return cats.slice(1,3)
 }
